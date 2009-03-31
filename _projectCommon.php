aa88a892d3492e154147135d0592b6ece47c2a09
jmcconnell <jmcconnell>
2009-03-31 13:39:35 +0000

    updating website

--- _projectCommon.php
+++ _projectCommon.php
@@ -11,11 +11,12 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->setLinkList( array() );
+	
     $Nav->addNavSeparator( "Jetty Home", "/jetty" );
-    $Nav->addCustomNav("About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=rt.jetty", "_self", 2);
-	$Nav->addCustomNav("Mailing Lists", "/jetty/mailinglists.php", "_self", 2);
+    #$Nav->addCustomNav("About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=rt.jetty", "_self", 3);
+	$Nav->addCustomNav("Mailing Lists", "/jetty/mailinglists.php", "_self", 1);
 	$Nav->addCustomNav("Newsgroup", "/jetty/newsgroup.php", "_self", 2);
-	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/Jetty", "_self", 2);
+	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/Jetty", "_self", 3);
 	$Nav->addCustomNav("Bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Jetty&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED", "_self", 2);
 
 ?>
