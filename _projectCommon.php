30b246efe10383b8103df6817b5bb5821ebb3775
jmcconnell <jmcconnell>
2009-03-31 13:40:58 +0000

    updating website

--- _projectCommon.php
+++ _projectCommon.php
@@ -14,9 +14,10 @@
 	
     $Nav->addNavSeparator( "Jetty Home", "/jetty" );
     #$Nav->addCustomNav("About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=rt.jetty", "_self", 3);
-	$Nav->addCustomNav("Mailing Lists", "/jetty/mailinglists.php", "_self", 1);
+    $Nav->addCustomNav("About", "/jetty/mailinglists.php", "_self", 2);
+	$Nav->addCustomNav("Mailing Lists", "/jetty/mailinglists.php", "_self", 2);
 	$Nav->addCustomNav("Newsgroup", "/jetty/newsgroup.php", "_self", 2);
-	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/Jetty", "_self", 3);
+	$Nav->addCustomNav("Wiki", "http://wiki.eclipse.org/Jetty", "_self", 2);
 	$Nav->addCustomNav("Bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Jetty&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED", "_self", 2);
 
 ?>
