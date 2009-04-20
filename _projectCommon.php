9ab3240711a10add01784d49642e5391dd4f912d
ayao <ayao>
2009-04-20 08:45:12 +0000

    Update project links separators

--- _projectCommon.php
+++ _projectCommon.php
@@ -21,7 +21,7 @@
     $Nav->addCustom("Jetty Powered", "http://docs.codehaus.org/display/JETTY/Jetty+Powered", "_self", 2);
     #FIXME: $Nav->addCustom("Licenses", "", "_self", 2);
 
-    $Nav->addNavSeparator("Resources");
+    $Nav->addNavSeparator("Resources", "/jetty");
     $Nav->addCustomNav("Downloads", "/jetty/downloads.php", "_self", 2);
     $Nav->addCustomNav("Documentation", "http://wiki.eclipse.org/Jetty", "_self", 2 );
     #FIXME: $Nav->addCustomNav("FAQ", "", "_self", 2 );
@@ -31,7 +31,7 @@
     
     #FIXME: $Nav->addNavSeparator("Plugins");
     
-    $Nav->addNavSeparator("Project Management");
+    $Nav->addNavSeparator("Project Management", "/jetty");
     $Nav->addCustomNav("Project Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=rt.jetty", "_self", 2 );
     $Nav->addCustomNav("Bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=Jetty&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED", "_self", 2 );
     $Nav->addCustomNav("IP Log", "http://www.eclipse.org/projects/ip_log.php?projectid=rt.jetty", "_self", 2 );
