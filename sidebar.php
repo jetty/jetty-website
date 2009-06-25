1ae3efb9a4e3b1ae795543f569ed02bab8dbbdf7
jmcconnell <jmcconnell>
2009-06-25 15:10:10 +0000

    remove incubation status and add release to project-plan

--- sidebar.php
+++ sidebar.php
@@ -2,11 +2,6 @@
     $sidebar = <<<EOHTML
     
 	<div class="sideitem">
-		<h6>Project Status: Incubation</h6>
-		<div align="center"><a href="/projects/gazoo.php"><img align="center" src="/images/egg-incubation.png" border="0"/></a></div>			
-	</div>	
-	
-	<div class="sideitem">
 		<h6>Quick Links</h6>
 		<ul>		
 			<li><a href="http://www.eclipse.org/projects/project_summary.php?projectid=rt.jetty">Project Summary</a></li>
