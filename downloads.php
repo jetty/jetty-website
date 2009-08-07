2bca03ad2ed0c53f3b5a4e0e7e07ea6acee04cb9
ayao <ayao>
2009-08-07 09:38:12 +0000

    Redo changes somehow got overwritten

--- downloads.php
+++ downloads.php
@@ -13,35 +13,35 @@
 	<div id="midcolumn">
 		<h1>Downloads</h1>
 		<div class="homeitem3col">	
-			<h2>Jetty Releases from Eclipse</h2>
+			<h2>Jetty Distributions from Eclipse</h2>
             <p>
-                Jetty milestone builds are now available.
+                The Jetty@eclipse project is unable to make full distributions of Jetty available for download from the Eclipse servers until all our dependencies (e.g., JSP) have been put through the Eclipse IP process.
             </p>            
-            <ul>
-               <li>
-                    <b>Jetty</b>: 7.0.0.RC1  |  <b>Date</b>: 15 July 2009  <br/>
-                    <img src="images/arrow.gif"></img>&nbsp;&nbsp;<b><a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.zip">jetty-distribution-7.0.0.RC1.zip</a></b>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.zip.md5"><i>MD5</i></a>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.zip.sha1"><i>SHA1</i></a><br/>
-                    <img src="images/arrow.gif"></img>&nbsp;&nbsp;<b><a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.tar.gz">jetty-distribution-7.0.0.RC1.tar.gz</a></b>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.tar.gz.md5"><i>MD5</i></a>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.tar.gz.sha1"><i>SHA1</i></a><br/>
-                    <img src="images/arrow.gif"></img>&nbsp;&nbsp;<b><a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.tar.bz2">jetty-distribution-7.0.0.RC1.tar.bz2</a></b>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.tar.bz2.md5"><i>MD5</i></a>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC1/dist/jetty-distribution-7.0.0.RC1.tar.bz2.sha1"><i>SHA1</i></a><br/>
-               </li>
-				   <li>
-                    <b>Jetty</b>: 7.0.0.RC0  |  <b>Date</b>: 9 July 2009  <br/>
-                    <img src="images/arrow.gif"></img>&nbsp;&nbsp;<b><a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.zip">jetty-distribution-7.0.0.RC0.zip</a></b>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.zip.md5"><i>MD5</i></a>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.zip.sha1"><i>SHA1</i></a><br/>
-                    <img src="images/arrow.gif"></img>&nbsp;&nbsp;<b><a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.tar.gz">jetty-distribution-7.0.0.RC0.tar.gz</a></b>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.tar.gz.md5"><i>MD5</i></a>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.tar.gz.sha1"><i>SHA1</i></a><br/>
-                    <img src="images/arrow.gif"></img>&nbsp;&nbsp;<b><a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.tar.bz2">jetty-distribution-7.0.0.RC0.tar.bz2</a></b>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.tar.bz2.md5"><i>MD5</i></a>&nbsp;&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.RC0/dist/jetty-distribution-7.0.0.RC0.tar.bz2.sha1"><i>SHA1</i></a><br/>
-               </li>            
-            </ul>
-            <p>Historical releases are available on the <a href="/jetty/previousDownloads.php">previous downloads</a> page.</p>
-            <p>The <a href="http://wiki.eclipse.org/Jetty/Reference/Index_of_Generated_Release_Documents">index of the JavaDoc and Source XRef links</a> for each milestone build is also available. 
-		</div>		
-		<p>
-            All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified. Please note that the use of the software you are about to access may be subject to third party terms and conditions and you are responsible for abiding by such terms and conditions.
-		</p>
+            <p>
+            	Jetty 7 JARs which have been through the Eclipse JAR conditioning process are available from the Jetty Eclipse OSGi update site:
+            </p>
+            <p>
+            	<a href=" http://download.eclipse.org/jetty/stable-7/update/">http://download.eclipse.org/jetty/stable-7/update/</a>
+            </p>
+			<p>
+				The unconditioned Jetty 7 JARs and Maven artifacts are also available from the central Maven repository:
+			</p>
+			<p>
+				 <a href="http://repo2.maven.org/maven2/org/eclipse/jetty/">http://repo2.maven.org/maven2/org/eclipse/jetty/</a>
+			</p>
+		</div>
 		<div class="homeitem3col">
-		  <h2>Jetty Releases from codehaus</h2>
-		  <p>
-		      Download Jetty accessories, integrations, extensions, or older versions of Jetty from the <a href="http://docs.codehaus.org/display/JETTY/Downloading+Jetty">Downloads</a> page at the codehaus.
-		  </p>
+			<h2>Jetty Distributions from codehaus</h2>
+			<p>
+				A Jetty distribution that combines the Jetty@eclipse JARs with additional components (JSP, JTA, etc.) is available via:
+				<ul>
+					<li><a href="http://dist.codehaus.org/jetty/">Jetty@codehaus project</a></li>
+					<li><a href="http://repo2.maven.org/maven2/org/mortbay/jetty/jetty-distribution/">Maven central repository</a></li>
+				</ul>
+			</p>		  
+			<p>
+				This distribution may include artifacts that are not covered by the terms and conditions of the Eclipse Foundation Software User Agreement. For details, please read the NOTICES file included in the distribution.
+			</p>		  
 		</div>
 		<h2>Frequently Asked Questions</h2>
 		<div id="install">
