51f235ae004ce49fbd76f369b5194a7393a400cd
jmcconnell <jmcconnell>
2010-10-06 14:16:51 +0000

    added paragraph page

---
+++ paragraph.php
@@ -0,0 +1,6 @@
+<p>Jetty provides an Web server and javax.servlet container, plus support for Web Sockets, OSGi, JMX, JNDI, JASPI, AJP and many other integrations. These components are open source and available for commercial use and distribution.</p>
+
+<p>Jetty is used in a wide variety of projects and products. Jetty can be embedded in devices, tools, frameworks, application servers, and clusters. See the Jetty Powered page for more uses of Jetty.</p>
+
+<p>The core Jetty project is hosted by the Eclipse Foundation. The codehaus provides Jetty accessories , integrations, and extensions, as well as hosting older versions of Jetty. See the About page for information about the project structure.</p>
+
