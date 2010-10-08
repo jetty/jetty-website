91014a7bedd82e064b76a315a8e47a4eeda9d98c
jmcconnell <jmcconnell>
2010-10-08 15:47:56 +0000

    shorter paragraph

--- paragraph.php
+++ paragraph.php
@@ -1,2 +1,2 @@
-<p>Jetty provides an Web server and javax.servlet container, plus support for Web Sockets, OSGi, JMX, JNDI, JASPI, AJP and many other integrations. These components are open source and available for commercial use and distribution.  Jetty is used in a wide variety of projects and products. Jetty can be embedded in devices, tools, frameworks, application servers, and clusters.</p>
+<p>Jetty provides an Web server and javax.servlet container, plus support for Web Sockets, OSGi, JMX, JNDI, JASPI, AJP and many other integrations. Jetty is used in a wide variety of projects and products. Jetty can be embedded in devices, tools, frameworks, application servers, and clusters.</p>
 
