9ab5787046696b7f8dd64e1dbeed30fef3841f64
Jesse McConnell <jesse.mcconnell@gmail.com>
2013-01-18 08:46:42 -0600

    futzing with php

--- about.php
+++ about.php
@@ -232,7 +232,7 @@ EOHTML;
 
 
 	# Generate the web page
-	$App->AddGoogleAnalyticsTrackingCode("UA-1149868-5");
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	$App->AddGoogleAnalyticsTrackingCode("UA-1149868-5");
 
 ?>
