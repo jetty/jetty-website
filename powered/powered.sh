bc5feaaec0c11fe417652d5fd1fc45f1d6e3c914
Jesse McConnell <jesse.mcconnell@gmail.com>
2013-04-11 15:33:33 -0500

    updates

--- powered/powered.sh
+++ powered/powered.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 {
-echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML+RDFa 1.0//EN\" \"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd\">"
+echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
 echo "<html xmlns=\"http://www.w3.org/1999/xhtml\">"
 echo "<head>"
 echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"/>"
