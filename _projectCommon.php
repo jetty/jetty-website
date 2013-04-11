ff44807b34e2f015a1bd96eae86e1b7f82c58a72
Jesse McConnell <jesse.mcconnell@gmail.com>
2013-04-11 10:52:40 -0500

    updates

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,7 +8,7 @@
     include("sidebar.php");
 
     # Additional styles
-    $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/jetty/css/jetty.css"/>' . "\n");
+#    $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/jetty/css/jetty.css"/>' . "\n");
     
     $App->SetGoogleAnalyticsTrackingCode("UA-1149868-7");
     # Define your project-wide Nav bars here.
@@ -17,27 +17,25 @@
     $Nav->setLinkList( array() );
     
     ## required by eclipse guidelines
-    $Nav->addNavSeparator("Jetty", null);
+    $Nav->addNavSeparator("Jetty", "/jetty/);
     $Nav->addCustomNav("About This Project", "/projects/project_summary.php?projectid=rt.jetty", "_self", 2); // required link for project :/
     $Nav->addCustomNav("About Jetty", "/jetty/about.php", "_self", 2);
     # custom links
-    # $Nav->addCustomNav("jetty@eclipse", "/jetty", "_self", 2);
-    # $Nav->addCustomNav("jetty@codehaus", "http://jetty.codehaus.org/jetty/", "_self", 2);
     $Nav->addCustomNav("Jetty Powered", "http://docs.codehaus.org/display/JETTY/Jetty+Powered", "_self", 2);
     $Nav->addCustomNav("Licenses", "/jetty/licenses.php", "_self", 2);
 
-    $Nav->addNavSeparator("Resources", null);
+    $Nav->addNavSeparator("Resources", "/jetty/");
     $Nav->addCustomNav("Documentation", "http://www.eclipse.org/jetty/documentation", "_self", 2);
     $Nav->addCustomNav("Downloads", "/jetty/downloads.php", "_self", 2);
     #FIXME: $Nav->addCustomNav("FAQ", "", "_self", 2);
     $Nav->addCustomNav("Mailing Lists", "/jetty/mailinglists.php", "_self", 2);
-    $Nav->addCustomNav("Jetty 9 JavaDoc", "http://download.eclipse.org/jetty/stable-9/apidocs/", "_self", 2);
-    $Nav->addCustomNav("Jetty 9 Source XRef", "http://download.eclipse.org/jetty/stable-9/xref/", "_self", 2);
+    $Nav->addCustomNav("JavaDoc", "http://download.eclipse.org/jetty/stable-9/apidocs/", "_self", 2);
+    $Nav->addCustomNav("Source XRef", "http://download.eclipse.org/jetty/stable-9/xref/", "_self", 2);
     $Nav->addCustomNav("Blogs", "http://webtide.intalio.org", "_self", 2);
     
     #FIXME: $Nav->addNavSeparator("Plugins", null);
     
-    $Nav->addNavSeparator("Project Management", null);
+    $Nav->addNavSeparator("Project Management", "/jetty/");
     $Nav->addCustomNav("Project Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=rt.jetty", "_self", 2);
     # $Nav->addCustomNav("Bugs", "https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&amp;short_desc_type=allwordssubstr&amp;short_desc=""&amp;product=Jetty&amp;long_desc_type=allwordssubstr&amp;long_desc=&amp;bug_file_loc_type=allwordssubstr&amp;bug_file_loc=&amp;status_whiteboard_type=allwordssubstr&amp;status_whiteboard=&amp;keywords_type=allwords&amp;keywords=&amp;bug_status=NEW&amp;bug_status=ASSIGNED&amp;bug_status=REOPENED", "_self", 2);
     $Nav->addCustomNav("IP Log", "http://www.eclipse.org/projects/ip_log.php?projectid=rt.jetty", "_self", 2);
@@ -45,3 +43,4 @@
     
 ?>
 
+<link rel="stylesheet" type="text/css" href="/jetty/css/jetty.css"/>
