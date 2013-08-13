3d29c8288140f456b01831cea13289982826559b
Jesse McConnell <jesse.mcconnell@gmail.com>
2013-08-13 09:34:36 -0500

    add yourkit blurb

---
+++ profiling.php
@@ -0,0 +1,35 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Eclipse RT - Jetty - Profiling";
+	$pageKeywords	= "Jetty, Servlets, Async, Web Server, Web Client, Mailing Lists, Eclipse RT, Eclipse Runtime";
+	$pageAuthor		= "jmcconnell";
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+	<p><img alt="Jetty Logo" src="/jetty/images/jetty-logo-80x22.png"/></p>
+		<h1>YourKit</h1>
+		<p>The Jetty project uses YourKit for profiling Jetty.</p>
+		<p>
+YourKit is kindly supporting Jetty open source projects with its full-featured Java Profiler.
+YourKit, LLC is the creator of innovative and intelligent tools for profiling
+Java and .NET applications. Take a look at YourKit's leading software products:
+<a href="http://www.yourkit.com/java/profiler/index.jsp">YourKit Java Profiler</a> and
+<a href="http://www.yourkit.com/.net/profiler/index.jsp">YourKit .NET Profiler</a>.
+		</p>
+	</div>	
+
+	<div id="rightcolumn">
+        $sidebar		
+	</div>
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
