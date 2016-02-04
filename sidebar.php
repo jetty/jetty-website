c29b9ae40a2f6412b85d630177ac153fae12b026
Jesse McConnell <jesse.mcconnell@gmail.com>
2016-02-04 10:23:15 -0600

    update logo

--- sidebar.php
+++ sidebar.php
@@ -16,7 +16,7 @@
     <h6>Active Contributors</h6>
     <div style="position: relative; height: 50px;">
       <a href="http://www.webtide.com/" target="_blank"
-          title="Webtide"><img alt="Webtide Logo" src="http://www.eclipse.org/jetty/images/webtide.png"
+          title="Webtide"><img alt="Webtide Logo" src="http://www.eclipse.org/jetty/images/webtide-dark.png"
           style="position: absolute; left: 10px; top: 10px; width: 150px; height: auto;"/></a>
     </div>
   </div>
