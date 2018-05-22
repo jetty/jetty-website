797e342fbc76d9d40aeef54cff87236cb66892b7
Jesse McConnell <jesse.mcconnell@gmail.com>
2018-05-22 10:42:26 -0500

    test

--- content/en_index.php
+++ content/en_index.php
@@ -20,16 +20,50 @@
     <p>The current recommended version for use is Jetty 9 which can be obtained on the Jetty Downloads page.<p>
     <p>The Jetty project has been hosted at the Eclipse Foundation since 2009. Prior releases of Jetty have existed in part or completely under the Jetty project at the The Codehaus and Sourceforge before that. See the About page for more information about the history of Jetty.</p>
 
-
+    <table class="table" summary="empty" border="0">
+      <colgroup>
+        <col class="col_1"><col class="col_2">
+      </colgroup>
+      <thead>
+        <tr>
+          <th align="left" valign="top">Features</th>
+          <th align="left" valign="top">Jetty Powered</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td align="left" valign="top">
+            <div class="itemizedlist">
+              <ul class="itemizedlist" style="list-style-type: disc; ">
+                <li class="listitem">Full-featured and standards-based</li>
+                <li class="listitem">Open source and commercially usable</li>
+                <li class="listitem">Flexible and extensible</li>
+                <li class="listitem">Small footprint</li>
+                <li class="listitem">Embeddable</li>
+                <li class="listitem">Asynchronous</li>
+                <li class="listitem">Enterprise scalable</li>
+                <li class="listitem">Dual licensed under Apache and Eclipse</li>
+              </ul>
+            </div>
+          </td>
+          <td align="left" valign="top">
+            <div class="itemizedlist">
+              <ul class="itemizedlist" style="list-style-type: disc; ">
+                <li class="listitem">Large clusters, such as the Yahoo Hadoop Cluster</li>
+                <li class="listitem">Cloud computing, such as the Google AppEngine</li>
+                <li class="listitem">SaaS, such as Yahoo! and Zimbra</li>
+                <li class="listitem">Application Servers, such as Apache Geronimo</li>
+                <li class="listitem">Frameworks, such as GWT</li>
+                <li class="listitem">Tools, such as the Eclipse IDE</li>
+                <li class="listitem">Devices, such as phones</li>
+                <li class="listitem">More&#8230;&#8203;</li>
+              </ul>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
     <p>You can benefit from committer knowledge and get training, consulting services, professional support and even production SLAs, just ask us about it!</p>
-  </div>
-</div>
-<div id="leftcolumn">
-  <div class="sideitem">
-    <h6>Quick Links</h6>
-    <div style="text-align: center">
-      <a href="/projects/project_summary.php?projectid=rt.jetty">Project Summary</a>
-    </div>
   </div>
 </div>
