b1dbf66f6db6fbc9383b8739b60faea19e2d235e
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-03-06 14:23:21 -0600

    php test

--- content/en_index.php
+++ content/en_index.php
@@ -17,7 +17,24 @@
 <!-- Main content area -->
 <div id="midcolumn">
   <h1><?php print $pageTitle;?></h1>
-  <p><a href="solstice-starterkit.zip" class="btn btn-warning">Download Starterkit</a></p>
+  <p>
+    Eclipse Jetty provides a Web server and javax.servlet container, plus support for HTTP/2, WebSocket, OSGi, JMX, JNDI, JAAS and many other integrations.
+    These components are open source and available for commercial use and distribution.
+  </p>
+  <p>
+    Eclipse Jetty is used in a wide variety of projects and products, both in development and production.
+    Jetty can be easily embedded in devices, tools, frameworks, application servers, and clusters.
+    See the Jetty Powered page for more uses of Jetty.
+  </p>
+  <p>
+    The current recommended version for use is Jetty 9 which can be obtained on the link:/jetty/download.html[Jetty Downloads page.]
+    Also available are the latest maintenance releases of Jetty 8 and Jetty 7,
+  </p>
+  <p>
+    The Jetty project has been hosted at the Eclipse Foundation since 2009.
+    Prior releases of Jetty have existed in part or completely under the Jetty project at the The Codehaus and Sourceforge before that.
+    See the <a href="/jetty/about.html">About</a> page for more information about the history of Jetty.
+  </p>
 </div>
 <!-- ./end  #midcolumn -->
 
