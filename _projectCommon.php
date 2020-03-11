a95222d7871e66a8de8f5718a51343656c624425
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-03-11 15:58:02 -0500

    php test

--- _projectCommon.php
+++ _projectCommon.php
@@ -47,7 +47,7 @@ $variables['hide_breadcrumbs'] = FALSE;
 $variables['leftnav_html'] = '';
 
 // Update the main container class (String)
-$variables['main_container_classes'] = 'container';
+$variables['main_container_classes'] = 'container-full';
 
 // Insert HTML after opening the main content container, before the left
 // sidebar. (String)
