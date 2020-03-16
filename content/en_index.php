a0f0c9143b54a70a0c4aae9596a54e259a082a9b
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-03-16 15:38:26 -0500

    test php

---
+++ content/en_index.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Copyright (c) 2014, 2015, 2016, 2018 Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *   Christopher Guindon (Eclipse Foundation) - Initial implementation
+ *   Eric Poirier (Eclipse Foundation)
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+?>
+
+<!-- Main content area -->
+<div id="midcolumn">
+  <h1><?php print $pageTitle;?></h1>
+  <div class="paragraph">
+  <p>Eclipse Jetty provides a Web server and javax.servlet container, plus support for HTTP/2, WebSocket, OSGi, JMX, JNDI, JAAS and many other integrations.
+  These components are open source and available for commercial use and distribution.</p>
+  </div>
+  <div class="paragraph">
+  <p>Eclipse Jetty is used in a wide variety of projects and products, both in development and production.
+  Jetty can be easily embedded in devices, tools, frameworks, application servers, and clusters.
+  See the Jetty Powered page for more uses of Jetty.</p>
+  </div>
+  <div class="paragraph">
+  <p>The current recommended version for use is Jetty 9 which can be obtained on the <a href="/jetty/download.html">Jetty Downloads page.</a>
+  Also available are the latest maintenance releases of Jetty 8 and Jetty 7,</p>
+  </div>
+  <div class="paragraph">
+  <p>The Jetty project has been hosted at the Eclipse Foundation since 2009.
+  Prior releases of Jetty have existed in part or completely under the Jetty project at the The Codehaus and Sourceforge before that.
+  See the <a href="/jetty/about.html">About</a> page for more information about the history of Jetty.</p>
+  </div>
+  <table class="tableblock frame-none grid-all stretch">
+  <colgroup>
+  <col style="width: 50%;">
+  <col style="width: 50%;">
+  </colgroup>
+  <thead>
+  <tr>
+  <th class="tableblock halign-left valign-top">Features</th>
+  <th class="tableblock halign-left valign-top">Jetty Powered</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr>
+  <td class="tableblock halign-left valign-top"><div class="content"><div class="ulist">
+  <ul>
+  <li>
+  <p>Full-featured and standards-based</p>
+  </li>
+  <li>
+  <p>Open source and commercially usable</p>
+  </li>
+  <li>
+  <p>Flexible and extensible</p>
+  </li>
+  <li>
+  <p>Small footprint</p>
+  </li>
+  <li>
+  <p>Embeddable</p>
+  </li>
+  <li>
+  <p>Asynchronous</p>
+  </li>
+  <li>
+  <p>Enterprise scalable</p>
+  </li>
+  <li>
+  <p>Dual licensed under Apache and Eclipse</p>
+  </li>
+  </ul>
+  </div></div></td>
+  <td class="tableblock halign-left valign-top"><div class="content"><div class="ulist">
+  <ul>
+  <li>
+  <p>Large clusters, such as the Facebook Presto</p>
+  </li>
+  <li>
+  <p>Cloud computing, such as the Google AppEngine</p>
+  </li>
+  <li>
+  <p>SaaS, such as Yahoo! and Zimbra</p>
+  </li>
+  <li>
+  <p>Application Servers, such as Apache Geronimo</p>
+  </li>
+  <li>
+  <p>Frameworks, such as GWT</p>
+  </li>
+  <li>
+  <p>Tools, such as the Eclipse IDE</p>
+  </li>
+  <li>
+  <p>Devices, such as phones</p>
+  </li>
+  <li>
+  <p>More&#8230;&#8203;</p>
+  </li>
+  </ul>
+  </div></div></td>
+  </tr>
+  </tbody>
+  </table>
+  <div class="paragraph">
+  <p>You can benefit from committer knowledge and get training, consulting services, professional support and even production SLAs, just ask us about it!</p>
+  </div>
+</div>
+<!-- ./end  #midcolumn -->
+
+<!-- Start of the right column -->
+<!--
+<div id="rightcolumn">
+  <div class="sideitem">
+    <h2>Related Links</h2>
+    <ul>
+      <li><a target="_self" href="/eclipse.org-common/themes/solstice/docs/">Documentation</a></li>
+    </ul>
+  </div>
+</div>
+-->
+<!-- ./end  #rightcolumn -->
