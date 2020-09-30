a39748af6bfc4578b0a42b0b61defb3fd140cc6c
Chris Walker <chris@webtide.com>
2020-09-30 15:09:33 -0500

    Documentation table expanded x12

--- content/en_documentation.php
+++ content/en_documentation.php
@@ -3,7 +3,7 @@
     <p>The latest documentation releases are Eclipse Jetty is below, earlier minor release versions are available in <a href="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-documentation">Maven Central.</a></p>
     <table class="frame-all grid-all stretch">
         <colgroup>
-            <col style="width: 35%;">
+            <col style="width: 15%;">
             <col style="width: 15%;">
             <col style="width: 15%;">
             <col style="width: 15%;">
@@ -26,7 +26,7 @@
         </thead>
         <tbody>
         <tr>
-            <td class="halign-left valign-top"><p><b>Jetty 11<</b>/p></td>
+            <td class="halign-left valign-top"><p><b>Jetty 11</b></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/contribution-guide/index.html">Contribution Guide</a></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/operations-guide/index.html">Operations Guide</a></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/programing-guide/index.html">Programming Guide</a></p></td>
