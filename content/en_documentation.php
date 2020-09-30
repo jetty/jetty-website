54aaf508381c66b2127537a31858b01df07bf552
Chris Walker <chris@webtide.com>
2020-09-30 15:06:17 -0500

    Documentation table expanded x11

--- content/en_documentation.php
+++ content/en_documentation.php
@@ -1,10 +1,9 @@
 <div id="midcolumn">
     <h1><?php print $pageTitle;?></h1>
     <p>The latest documentation releases are Eclipse Jetty is below, earlier minor release versions are available in <a href="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-documentation">Maven Central.</a></p>
-    <table class="frame-all " cellspacing="15">
-        <caption class="title">Downloads</caption>
+    <table class="frame-all grid-all stretch">
         <colgroup>
-            <col style="width: 40%;">
+            <col style="width: 35%;">
             <col style="width: 15%;">
             <col style="width: 15%;">
             <col style="width: 15%;">
@@ -27,7 +26,7 @@
         </thead>
         <tbody>
         <tr>
-            <td class="halign-left valign-top"><p>Jetty 11</p></td>
+            <td class="halign-left valign-top"><p><b>Jetty 11<</b>/p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/contribution-guide/index.html">Contribution Guide</a></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/operations-guide/index.html">Operations Guide</a></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/programing-guide/index.html">Programming Guide</a></p></td>
@@ -37,7 +36,7 @@
             <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> source</a></p></td>
         </tr>
         <tr>
-            <td class="halign-left valign-top"><p>Jetty 10</p></td>
+            <td class="halign-left valign-top"><p><b>Jetty 10</b></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/contribution-guide/index.html">Contribution Guide</a></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/operations-guide/index.html">Operations Guide</a></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/programing-guide/index.html">Programming Guide</a></p></td>
@@ -47,7 +46,7 @@
             <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> source</a></p></td>
         </tr>
         <tr>
-            <td class="halign-left valign-top"><p>Jetty 9</p></td>
+            <td class="halign-left valign-top"><p><b>Jetty 9</p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/documentation/9.4.x/index.html">Central Documentation</a></p></td>
             <td class="halign-left valign-top"><p><a href="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.4.31.v20200723/jetty-distribution-9.4.31.v20200723.zip">.zip</a></p></td>
             <td class="halign-left valign-top"><p><a href="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.4.31.v20200723/jetty-distribution-9.4.31.v20200723.tar.gz">.tgz</a></p></td>
