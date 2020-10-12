cacac6d4998e2a185482ffcf193d3da458054147
Chris Walker <chris@webtide.com>
2020-10-12 12:30:08 -0500

    Working on Documentation Page Mk.III

--- content/en_documentation.php
+++ content/en_documentation.php
@@ -10,10 +10,10 @@
         </colgroup>
         <thead>
         <tr>
-            <th class="halign-left valign-top">Release</th>
+            <th class="halign-left valign-top">Release Version</th>
             <th class="halign-left valign-top"></th>
-            <th class="halign-left valign-top">API Documentation</th>
-            <th class="halign-left valign-top">Source Code</th>
+            <th class="halign-left valign-top"></th>
+            <th class="halign-left valign-top"></th>
         </tr>
         </thead>
         <tbody>
@@ -21,20 +21,26 @@
             <td class="halign-left valign-top"><p><b>Jetty 11</b></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/documentation/jetty-11/index.html">Documentation</a></p></td>
             <td class="halign-left valign-top"><p><a href="http://www.eclipse.org/jetty/javadoc/9.4.31.v20200723/index.html?overview-summary.html">API </a></p></td>
-            <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> source</a></p></td>
+            <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> Source Code</a></p></td>
         </tr>
         <tr>
             <td class="halign-left valign-top"><p><b>Jetty 10</b></p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/documentation/jetty-10/index.html">Documentation</a></p></td>
             <td class="halign-left valign-top"><p><a href="http://www.eclipse.org/jetty/javadoc/9.4.31.v20200723/index.html?overview-summary.html">API </a></p></td>
-            <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> source</a></p></td>
+            <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> Source Code</a></p></td>
         </tr>
         <tr>
             <td class="halign-left valign-top"><p><b>Jetty 9</p></td>
             <td class="halign-left valign-top"><p><a href="/jetty/documentation/9.4.x/index.html">Documentation</a></p></td>
             <td class="halign-left valign-top"><p><a href="http://www.eclipse.org/jetty/javadoc/9.4.31.v20200723/index.html?overview-summary.html">API </a></p></td>
-            <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> source</a></p></td>
+            <td class="halign-left valign-top"><p><a href="https://github.com/eclipse/jetty.project/tree/jetty-9.4.31.v20200723"> Source Code</a></p></td>
         </tr>
         </tbody>
     </table>
+
+    <div class="sect2">
+        <h2 id="_prior_versions">Previous Versions</h2>
+        <p>The most recent versions of prior Jetty releases can be found <a href="previousversions.html">here, with their associated documentation.</a></p>
+        <p>Note: The canonical repository for Jetty is Maven Central.  All releases are always available there first and this download page may lag a bit update wise as post release resources are put into place.  You can always browse for Jetty releases <a href="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution">here</a>.</p>
+    </div>
   </div>
