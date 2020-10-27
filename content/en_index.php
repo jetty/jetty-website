d8179a2a61c2569de60d2c4dcd0d2dd99645175b
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-10-27 12:39:30 -0500

    reformat and rearrange

--- content/en_index.php
+++ content/en_index.php
@@ -16,136 +16,177 @@
 
 <!-- Main content area -->
 <div id="midcolumn">
-  <h1><?php print $pageTitle;?></h1>
-  <p>Eclipse Jetty provides a web server and servlet container, additionally providing support for HTTP/2, WebSocket, OSGi, JMX, JNDI, JAAS and many other integrations.
-  These components are open source and are freely available for commercial use and distribution.</p>
+    <h1><?php print $pageTitle; ?></h1>
+    <p>Eclipse Jetty provides a web server and servlet container, additionally providing support for HTTP/2, WebSocket,
+        OSGi, JMX, JNDI, JAAS and many other integrations.
+        These components are open source and are freely available for commercial use and distribution.</p>
 
-  <p>Jetty is used in a wide variety of projects and products, both in development and production.
-  Jetty has long been loved by developers due to its long history of being easily embedded in devices, tools, frameworks,
-  application servers, and modern cloud services.</p>
+    <p>Jetty is used in a wide variety of projects and products, both in development and production.
+        Jetty has long been loved by developers due to its long history of being easily embedded in devices, tools,
+        frameworks,
+        application servers, and modern cloud services.</p>
 
-  <p>With the direction of Java and the JakartaEE project (formerly JavaEE) in 2020, the current
-      recommended version of Jetty for use depends upon the servlet api version, desired licensing and package namespace as well as the intended Java version.</p>
+
+    <table class="frame-none grid-all stretch">
+        <colgroup>
+            <col style="width: 50%;">
+            <col style="width: 50%;">
+        </colgroup>
+        <thead>
+        <tr>
+            <th class="halign-center valign-top">Features</th>
+            <th class="halign-center valign-top">Jetty Powered</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="halign-left valign-top">
+                <div class="content">
+                    <div class="ulist">
+                        <ul>
+                            <li>
+                                <p>Full-featured and standards-based</p>
+                            </li>
+                            <li>
+                                <p>Open source and commercially usable</p>
+                            </li>
+                            <li>
+                                <p>Flexible and extensible</p>
+                            </li>
+                            <li>
+                                <p>Small footprint</p>
+                            </li>
+                            <li>
+                                <p>Embeddable</p>
+                            </li>
+                            <li>
+                                <p>Asynchronous</p>
+                            </li>
+                            <li>
+                                <p>Enterprise scalable</p>
+                            </li>
+                            <li>
+                                <p>Dual licensed under Apache and Eclipse</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">
+                    <div class="ulist">
+                        <ul>
+                            <li>
+                                <p>Large clusters, such as the Facebook Presto</p>
+                            </li>
+                            <li>
+                                <p>Cloud computing, such as the Google AppEngine</p>
+                            </li>
+                            <li>
+                                <p>SaaS, such as Yahoo! and Zimbra</p>
+                            </li>
+                            <li>
+                                <p>Application Servers, such as Apache Geronimo</p>
+                            </li>
+                            <li>
+                                <p>Frameworks, such as GWT</p>
+                            </li>
+                            <li>
+                                <p>Tools, such as the Eclipse IDE</p>
+                            </li>
+                            <li>
+                                <p>Devices, such as Android</p>
+                            </li>
+                            <li>
+                                <p>More&#8230;&#8203;</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
+    <p>With the direction of Java and the JakartaEE project (formerly JavaEE) in 2020, the current
+        recommended version of Jetty for use depends upon the servlet api version, desired licensing and package
+        namespace as well as the intended Java version.</p>
 
     <br/>
     <table class="frame-none grid-all stretch">
-    <colgroup>
-        <col style="width: 25%;">
-        <col style="width: 25%;">
-        <col style="width: 25%;">
-        <col style="width: 25%;">
-    </colgroup>
-    <thead>
-    <tr>
-        <th class="halign-left valign-top">Jetty Version</th>
-        <th class="halign-left valign-top">Servlet</th>
-        <th class="halign-left valign-top">License</th>
-        <th class="halign-left valign-top">Java</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td class="halign-left valign-top"><div class="content">Jetty 9.4.x</div></td>
-        <td class="halign-left valign-top"><div class="content">3.1</div></td>
-        <td class="halign-left valign-top"><div class="content">JavaEE 8/javax.servlet.*</div></td>
-        <td class="halign-left valign-top"><div class="content">Java 8</div></td>
-    </tr>
-    <tr>
-        <td class="halign-left valign-top"><div class="content">Jetty 10.0.x</div></td>
-        <td class="halign-left valign-top"><div class="content">4.0</div></td>
-        <td class="halign-left valign-top"><div class="content">JakartaEE 8/javax.servlet.*</div></td>
-        <td class="halign-left valign-top"><div class="content">Java 11+</div></td>
-    </tr>
-    <tr>
-        <td class="halign-left valign-top"><div class="content">Jetty 11.0.x</div></td>
-        <td class="halign-left valign-top"><div class="content">5.0</div></td>
-        <td class="halign-left valign-top"><div class="content">JakartaEE 9/jakartaee.servlet.*</div></td>
-        <td class="halign-left valign-top"><div class="content">Java 11+</div></td>
-     </tr>
-    </tbody>
-   </table>
+        <colgroup>
+            <col style="width: 25%;">
+            <col style="width: 20%;">
+            <col style="width: 35%;">
+            <col style="width: 20%;">
+        </colgroup>
+        <thead>
+        <tr>
+            <th class="halign-left valign-top">Jetty Version</th>
+            <th class="halign-left valign-top">Servlet</th>
+            <th class="halign-left valign-top">License</th>
+            <th class="halign-left valign-top">Java</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="halign-left valign-top">
+                <div class="content">Jetty 9.4.x</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">3.1</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">JavaEE 8/javax.servlet.*</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">Java 8</div>
+            </td>
+        </tr>
+        <tr>
+            <td class="halign-left valign-top">
+                <div class="content">Jetty 10.0.x</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">4.0</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">JakartaEE 8/javax.servlet.*</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">Java 11+</div>
+            </td>
+        </tr>
+        <tr>
+            <td class="halign-left valign-top">
+                <div class="content">Jetty 11.0.x</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">5.0</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">JakartaEE 9/jakartaee.servlet.*</div>
+            </td>
+            <td class="halign-left valign-top">
+                <div class="content">Java 11+</div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
 
-    <!--
-    <p>For more information related to help you make an informed decision, please check the <a href="/jetty/versions.html">LOCATION NEEDED</a>.</p>
--->
-  <p>The Jetty project has been hosted at the Eclipse Foundation since 2009.
-  Prior releases of Jetty have existed in part or completely under the Jetty project at the The Codehaus and Sourceforge before that.
-  See the <a href="/jetty/about.html">About</a> page for more information about the history of Jetty.</p>
+    <p>For more information related to help you make an informed decision, please check the <a
+                href="/jetty/TODO.html">TODO</a>.</p>
 
-  <table class="frame-none grid-all stretch">
-  <colgroup>
-  <col style="width: 50%;">
-  <col style="width: 50%;">
-  </colgroup>
-  <thead>
-  <tr>
-  <th class="halign-left valign-top">Features</th>
-  <th class="halign-left valign-top">Jetty Powered</th>
-  </tr>
-  </thead>
-  <tbody>
-  <tr>
-  <td class="halign-left valign-top"><div class="content"><div class="ulist">
-  <ul>
-  <li>
-  <p>Full-featured and standards-based</p>
-  </li>
-  <li>
-  <p>Open source and commercially usable</p>
-  </li>
-  <li>
-  <p>Flexible and extensible</p>
-  </li>
-  <li>
-  <p>Small footprint</p>
-  </li>
-  <li>
-  <p>Embeddable</p>
-  </li>
-  <li>
-  <p>Asynchronous</p>
-  </li>
-  <li>
-  <p>Enterprise scalable</p>
-  </li>
-  <li>
-  <p>Dual licensed under Apache and Eclipse</p>
-  </li>
-  </ul>
-  </div></div></td>
-  <td class="halign-left valign-top"><div class="content"><div class="ulist">
-  <ul>
-  <li>
-  <p>Large clusters, such as the Facebook Presto</p>
-  </li>
-  <li>
-  <p>Cloud computing, such as the Google AppEngine</p>
-  </li>
-  <li>
-  <p>SaaS, such as Yahoo! and Zimbra</p>
-  </li>
-  <li>
-  <p>Application Servers, such as Apache Geronimo</p>
-  </li>
-  <li>
-  <p>Frameworks, such as GWT</p>
-  </li>
-  <li>
-  <p>Tools, such as the Eclipse IDE</p>
-  </li>
-  <li>
-  <p>Devices, such as Android</p>
-  </li>
-  <li>
-  <p>More&#8230;&#8203;</p>
-  </li>
-  </ul>
-  </div></div></td>
-  </tr>
-  </tbody>
-  </table>
-  <p>You can benefit from committer knowledge and get training, consulting services, professional support and even production SLAs, just ask us about it!</p>
-  <p></p>
-  <p></p>
+    <br/>
+
+    <p>The Jetty project has been hosted at the Eclipse Foundation since 2009.
+        Prior releases of Jetty have existed in part or completely under the Jetty project at the The Codehaus and
+        Sourceforge before that.
+        See the <a href="/jetty/about.html">About</a> page for more information about the history of Jetty.</p>
+
+    <p>You can benefit from committer knowledge and get training, consulting services, professional support and even
+        production SLAs, just ask us about it!</p>
+    <p></p>
+    <p></p>
 </div>
 <!-- ./end  #midcolumn -->
