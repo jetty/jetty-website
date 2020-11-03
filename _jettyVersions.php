416cb05ae76bd7a865729b15d1357121c93919c5
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-11-03 12:39:02 -0600

    fiddle with download page and some structure
    Signed-off-by: Jesse McConnell <jesse.mcconnell@gmail.com>

--- _jettyVersions.php
+++ _jettyVersions.php
@@ -1,6 +1,19 @@
 <?php
+/**
+ * Copyright (c) 2014-2017, 2018 Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *   Christopher Guindon (Eclipse Foundation) - Initial implementation
+ *   Eric Poirier (Eclipse Foundation)
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 
-$jetty = array(
+$jetty = array (
     'jetty_9.2' => array (
         'version' => '9.2.30.v20200428',
         'zip.md5' => 'fb9aa271289360caa00651fc3e348f90',
@@ -13,7 +26,7 @@ $jetty = array(
         'tgz.md5' => 'bdc54c7560d831db88eb4f3491cdc6e2'
     ),
 
-    'jetty_9.4' => array(
+    'jetty_9.4' => array (
         'version' => '9.4.33.v20201020',
         'zip.md5' => 'c15d443f76ed2b379c6469e0bc041d2d',
         'tgz.md5' => '0e1e3d160048285881fea426c878497d'
