fd2796e8ee2b9a948984e999cbbd79e0510631ae
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-11-10 10:56:42 -0600

    wire in documentation with iframe
    Signed-off-by: Jesse McConnell <jesse.mcconnell@gmail.com>

--- documentation.php
+++ documentation.php
@@ -33,12 +33,7 @@ if (isset($Nav)) {
 
 // Place your html content in a file called content/en_pagename.php
 ob_start();
-
-
-print "File: " . "content/en_10_" . $App->getScriptName();
-print "Changed: " . date("F d Y H:i:s.", filemtime("content/en_10_" . $App->getScriptName()));
-
-//include ("content/en_" . $App->getScriptName());
+include ("content/en_" . $App->getScriptName());
 $html = ob_get_clean();
 $Theme->setHtml($html);
 
