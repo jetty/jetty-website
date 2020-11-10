b455ea8993d8f7dd5191508580d618d15517326b
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-11-10 09:36:19 -0600

    wire in documentation with iframe
    Signed-off-by: Jesse McConnell <jesse.mcconnell@gmail.com>

--- contribution_guide.php
+++ contribution_guide.php
@@ -32,12 +32,12 @@ if (isset($Nav)) {
     $Theme->setNav($Nav);
 }
 
-/*
+
 $variables = array();
 $variables['main_container_classes'] = 'container-full';
 $variables['header_nav'] = "";
 $Theme->setThemeVariables($variables);
-*/
+
 
 // Place your html content in a file called content/en_pagename.php
 ob_start();
