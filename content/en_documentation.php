2e1681472053298574e3353a7ee122c15644a193
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-11-10 16:11:48 -0600

    wire in documentation with iframe
    Signed-off-by: Jesse McConnell <jesse.mcconnell@gmail.com>

--- content/en_documentation.php
+++ content/en_documentation.php
@@ -1,3 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2014, 2015, 2016, 2018 Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *   Christopher Guindon (Eclipse Foundation) - Initial implementation
+ *   Eric Poirier (Eclipse Foundation)
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+?>
+
 <div id="midcolumn">
     <h1><?php print $pageTitle;?></h1>
     <p>The latest documentation releases are Eclipse Jetty is below, earlier minor release versions are available in <a href="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-documentation">Maven Central.</a></p>
