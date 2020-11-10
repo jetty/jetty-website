5130b5d3ca6063faab119a8a123568364ad0d7b5
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-11-10 07:44:57 -0600

    wire in documentation with iframe
    Signed-off-by: Jesse McConnell <jesse.mcconnell@gmail.com>

--- content/en_documentation.php
+++ content/en_documentation.php
@@ -1,6 +1,7 @@
 <div id="midcolumn">
     <h1><?php print $pageTitle;?></h1>
     <p>The latest documentation releases are Eclipse Jetty is below, earlier minor release versions are available in <a href="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-documentation">Maven Central.</a></p>
+
     <table class="frame-all grid-all stretch">
         <colgroup>
             <col style="width: 25%;">
