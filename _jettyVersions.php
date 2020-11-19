bba9794e543b5e30ba8b67cd772f3549ab6e341a
Jesse McConnell <jesse.mcconnell@gmail.com>
2020-11-19 16:13:11 -0600

    add scripting to generate version info for site and position documentation
    Signed-off-by: Jesse McConnell <jesse.mcconnell@gmail.com>

--- _jettyVersions.php
+++ _jettyVersions.php
@@ -1,47 +1,50 @@
 <?php
 /**
- * Copyright (c) 2014-2017, 2018 Eclipse Foundation.
- *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * Contributors:
- *   Christopher Guindon (Eclipse Foundation) - Initial implementation
- *   Eric Poirier (Eclipse Foundation)
- *
- * SPDX-License-Identifier: EPL-2.0
- */
-
+* Copyright (c) 2020 Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* Contributors:
+*   Jesse McConnell
+*
+* SPDX-License-Identifier: EPL-2.0
+*/
 $jetty = array (
-    'jetty_9.2' => array (
-        'version' => '9.2.30.v20200428',
-        'zip.md5' => 'fb9aa271289360caa00651fc3e348f90',
-        'tgz.md5' => 'b2a24a5989cc6c1fd9ad7af630e6e0bb'
-    ),
-
-    'jetty_9.3' => array (
-        'version' => '9.3.29.v20201019',
-        'zip.md5' => '44847646d9821d6c8d252d6e761c96a9',
-        'tgz.md5' => 'bdc54c7560d831db88eb4f3491cdc6e2'
-    ),
-
-    'jetty_9.4' => array (
-        'version' => '9.4.33.v20201020',
-        'zip.md5' => 'c15d443f76ed2b379c6469e0bc041d2d',
-        'tgz.md5' => '0e1e3d160048285881fea426c878497d'
-    ),
-
-    'jetty_10.0' => array (
-        'version' => '10.0.0.beta3',
-        'zip.md5' => '01af155deb84d4aa8fe7e7b3f5ccbdd9',
-        'tgz.md5' => '738acfbd802b994d51319d228d1a7ee0'
-    ),
-
-    'jetty_11.0' => array (
-        'version' => '11.0.0.beta3',
-        'zip.md5' => '678bd612dc6d65579fe50ac7d1757f7a',
-        'tgz.md5' => 'a9a8498248f08e5c44b2b99d6ea1e3e7'
-    )
+ 'jetty_9.2' => array (
+    'version' => '9.2.30.v20200428',
+    'zip.md5' => 'fb9aa271289360caa00651fc3e348f90',
+    'zip.sha1' => '195c08bb43362f79c9c0d29c2d80c0c62e5581e0',
+    'tgz.md5' => 'b2a24a5989cc6c1fd9ad7af630e6e0bb',
+    'tgz.sha1' => '320737b6594dd6ee17bf4f79cf61b001ba8c3245',
+  ),
+ 'jetty_9.3' => array (
+    'version' => '9.3.29.v20201019',
+    'zip.md5' => '44847646d9821d6c8d252d6e761c96a9',
+    'zip.sha1' => 'acf3118a5cbfb3a6dfe218d3f20ecff09974d921',
+    'tgz.md5' => 'bdc54c7560d831db88eb4f3491cdc6e2',
+    'tgz.sha1' => '1f61ece21ad87c9835be9d9440725698b6928baa',
+  ),
+ 'jetty_9.4' => array (
+    'version' => '9.4.33.v20201020',
+    'zip.md5' => 'c15d443f76ed2b379c6469e0bc041d2d',
+    'zip.sha1' => '2b8e87c917672903ee01d1a45b6a0ae1fd468128',
+    'tgz.md5' => '0e1e3d160048285881fea426c878497d',
+    'tgz.sha1' => '365e0d8f311ff04339e63bc7463a63a6cabd99e9',
+  ),
+ 'jetty_10.0' => array (
+    'version' => '10.0.0.beta3',
+    'zip.md5' => '01af155deb84d4aa8fe7e7b3f5ccbdd9',
+    'zip.sha1' => 'b08911622619d0c569599ea026b644b9bdd81b66',
+    'tgz.md5' => '738acfbd802b994d51319d228d1a7ee0',
+    'tgz.sha1' => '05f75cad7f887116256c449f2f12c3730455ba10',
+  ),
+ 'jetty_11.0' => array (
+    'version' => '11.0.0.beta3',
+    'zip.md5' => '678bd612dc6d65579fe50ac7d1757f7a',
+    'zip.sha1' => '1d450bbccd714639ccdce7a8389d4f1433073e29',
+    'tgz.md5' => 'a9a8498248f08e5c44b2b99d6ea1e3e7',
+    'tgz.sha1' => 'a00a5d317669a5f28fd1846449d91e57db34f2d2',
+  )
 );
-
--- _update_for_releases.sh
+++ _update_for_releases.sh
@@ -1,57 +1,269 @@
 #!/usr/bin/env bash
 
-version_file="_jettyVersions.php";
+function usage() {
+  echo "Usage: "
+  echo " ./update_for_releases.sh"
+  echo "   -s : configured settings"
+  echo "   -c : clean archive of unused versions"
+  echo "   -u : update"
+}
 
-while IFS= read line
-do
-  if [[ $line == *"jetty_9.2_"* ]]; then
-    p1=${line#* \'};
-    p2=${p1%\'\;};
-    jetty_9_2=$p2;
+function set_global_variables() {
+  VERSIONS_TXT=$(pwd)/_jettyVersions.txt
+  VERSIONS_PHP=$(pwd)/_jettyVersions.php
+  ARC_DIR=$(pwd)/_archive
+  TEMP_DIR=$ARC_DIR/temp
+  MAVEN_ROOT="https://repo1.maven.org/maven2/org/eclipse/jetty"
+  DOC_ROOT="$MAVEN_ROOT/jetty-documentation"
+  LOG_FILE="$ARC_DIR/update.log"
+}
+
+function print_global_variables() {
+  echo "Versions File (txt): $VERSIONS_TXT"
+  echo "Versions File (php): $VERSIONS_PHP"
+  echo "Archive Directory: $ARC_DIR"
+  echo "Maven Root URL: $MAVEN_ROOT"
+  echo "Documentation Root URL: $DOC_ROOT"
+}
+
+function print_execution_variables() {
+  # TODO make dynamic
+  echo "Jetty 9.2 = $jetty_9_2"
+  echo "Jetty 9.3 = $jetty_9_3"
+  echo "Jetty 9.4 = $jetty_9_4"
+  echo "Jetty 10.0 = $jetty_10_0"
+  echo "Jetty 11.0 = $jetty_11_0"
+}
+
+function gather_current_versions() {
+
+  version_file="_jettyVersions.txt"
+
+  # TODO make dynamic
+  # shellcheck disable=SC2162
+  while IFS= read line; do
+    if [[ $line == *"9.2."* ]]; then
+      jetty_9_2=$line
+    fi
+
+    if [[ $line == *"9.3."* ]]; then
+      jetty_9_3=$line
+    fi
+
+    if [[ $line == *"9.4."* ]]; then
+      jetty_9_4=$line
+    fi
+
+    if [[ $line == *"10.0."* ]]; then
+      jetty_10_0=$line
+    fi
+
+    if [[ $line == *"11.0."* ]]; then
+      jetty_11_0=$line
+    fi
+
+  done <"$version_file"
+
+}
+
+#
+# set up archive directory if it doesn't exist
+#
+function create_archive_directory() {
+
+  if [ ! -d "$ARC_DIR" ]; then
+    mkdir "$ARC_DIR"
   fi
 
-  if [[ $line == *"jetty_9.4_"* ]]; then
-    p1=${line#* \'};
-    p2=${p1%\'\;};
-    jetty_9_4=$p2;
+}
+
+function create_temp_directory() {
+
+  rm -Rf "$TEMP_DIR"
+
+  if [ ! -d "$TEMP_DIR" ]; then
+    mkdir "$TEMP_DIR"
+  fi
+}
+
+#
+# we want to clear out files that are no longer required based on the current versions
+#
+function clean_archive_directory() {
+
+  # TODO implement
+  if [ -d "$ARC_DIR" ]; then
+    rm -R "$ARC_DIR"
   fi
 
-  if [[ $line == *"jetty_10.0_"* ]]; then
-    p1=${line#* \'};
-    p2=${p1%\'\;};
-    jetty_10_0=$p2;
+}
+
+function download() {
+  artifact=$1
+  version=$2
+  filename=$3
+
+  if [[ ! -f "$ARC_DIR/$filename" ]]; then
+    echo "  downloading $filename"
+    wget -O "$ARC_DIR/$filename" "$MAVEN_ROOT/$artifact/$version/$filename" &>>"$LOG_FILE"
+  #else
+  #  echo "  found: $filename";
+  fi
+}
+
+function download_distribution_files() {
+  version=$1
+
+  if [[ "$version" == "1"* ]]; then
+    artifact="jetty-home"
+  else
+    artifact="jetty-distribution"
   fi
 
-  if [[ $line == *"jetty_11.0_"* ]]; then
-    p1=${line#* \'};
-    p2=${p1%\'\;};
-    jetty_11_0=$p2;
+  filename_zasc="$artifact-$version.zip.asc"
+  filename_zmd5="$artifact-$version.zip.md5"
+  filename_zsha1="$artifact-$version.zip.sha1"
+  filename_tasc="$artifact-$version.tar.gz.asc"
+  filename_tmd5="$artifact-$version.tar.gz.md5"
+  filename_tsha1="$artifact-$version.tar.gz.sha1"
+  download "$artifact" "$version" "$filename_zasc"
+  download "$artifact" "$version" "$filename_zmd5"
+  download "$artifact" "$version" "$filename_zsha1"
+  download "$artifact" "$version" "$filename_tasc"
+  download "$artifact" "$version" "$filename_tmd5"
+  download "$artifact" "$version" "$filename_tsha1"
+}
+
+function download_documentation_files() {
+  artifact="jetty-documentation"
+  version=$1
+  filename="$artifact-$version-html.zip"
+  download "$artifact" "$version" "$filename"
+}
+
+function download_missing_files() {
+  create_archive_directory
+
+  # TODO make dynamic
+  # Jetty 9.2
+  download_distribution_files "$jetty_9_2"
+
+  # Jetty 9.3
+  download_distribution_files "$jetty_9_3"
+
+  # Jetty 9.4
+  download_distribution_files "$jetty_9_4"
+  download_documentation_files "$jetty_9_4"
+
+  # Jetty 10.0
+  download_distribution_files "$jetty_10_0"
+  download_documentation_files "$jetty_10_0"
+
+  # Jetty 11.0
+  download_distribution_files "$jetty_11_0"
+  download_documentation_files "$jetty_11_0"
+}
+
+function get_dist_info() {
+  if [[ "$1" == "1"* ]]; then
+    cat "$ARC_DIR/jetty-home-$1.$2"
+  else
+    cat "$ARC_DIR/jetty-distribution-$1.$2"
+  fi
+}
+
+function generate_version_php() {
+
+  # shellcheck disable=SC2206
+  # TODO make dynamic
+  versions=($jetty_9_2 $jetty_9_3 $jetty_9_4 $jetty_10_0 $jetty_11_0)
+
+  rm "$VERSIONS_PHP"
+
+  {
+    echo "<?php"
+    echo "/**"
+    echo "* Copyright (c) 2020 Eclipse Foundation."
+    echo "*"
+    echo "* This program and the accompanying materials are made"
+    echo "* available under the terms of the Eclipse Public License 2.0"
+    echo "* which is available at https://www.eclipse.org/legal/epl-2.0/"
+    echo "*"
+    echo "* Contributors:"
+    echo "*   Jesse McConnell"
+    echo "*"
+    echo "* SPDX-License-Identifier: EPL-2.0"
+    echo "*/"
+    echo "\$jetty = array ("
+    for version in "${versions[@]}"; do
+      # shellcheck disable=SC2206
+      IFS='.' read -ra ver <<<"$version"
+      # shellcheck disable=SC2128
+      # shellcheck disable=SC2211
+      echo " 'jetty_${ver[0]}.${ver[1]}' => array ("
+      echo "    'version' => '$version',"
+      echo "    'zip.md5' => '$(get_dist_info "$version" "zip.md5")',"
+      echo "    'zip.sha1' => '$(get_dist_info "$version" "zip.sha1")',"
+      #echo "    'zip.asc' => '$(get_dist_info "$version" "zip.asc")',";
+      echo "    'tgz.md5' => '$(get_dist_info "$version" "tar.gz.md5")',"
+      echo "    'tgz.sha1' => '$(get_dist_info "$version" "tar.gz.sha1")',"
+      #echo "    'tgz.asc' => '$(get_dist_info "$version" "tgz.asc")',";
+      if [ "$version" == "${versions[-1]}" ]; then
+        echo "  )"
+      else
+        echo "  ),"
+      fi
+    done
+    echo ");"
+  } >>"$VERSIONS_PHP"
+}
+
+function process_documentation() {
+  # shellcheck disable=SC2206
+  versions=($jetty_9_4 $jetty_10_0 $jetty_11_0)
+  create_temp_directory
+
+  for version in "${versions[@]}"; do
+    temp_ver_dir="$TEMP_DIR/$version";
+    unzip -d "$temp_ver_dir" "$ARC_DIR/jetty-documentation-$version-html.zip" &>>"$LOG_FILE";
+  done
+
+  # TODO make dynamic
+  {
+    rsync -avh "$TEMP_DIR/$jetty_9_4/$jetty_9_4" "$(pwd)/documentation/jetty-9" --delete;
+    rsync -avh "$TEMP_DIR/$jetty_10_0/$jetty_10_0/" "$(pwd)/documentation/jetty-10";
+    rsync -avh "$TEMP_DIR/$jetty_11_0/$jetty_11_0/" "$(pwd)/documentation/jetty-11";
+  } &>>"$LOG_FILE"
+}
+
+#
+# main
+#
+function main() {
+  directive=$1
+
+  # print out the settings this script operates under
+  if [[ $directive == "-s" ]]; then
+    set_global_variables
+    gather_current_versions
+    print_global_variables
+    print_execution_variables
+    exit
   fi
 
-done <"$version_file";
+  # run an update process for any version changes
+  if [[ $directive == "-u" ]]; then
+    set_global_variables
+    gather_current_versions
+    download_missing_files
+    generate_version_php
+    process_documentation
+    exit
+  fi
 
-echo "Jetty 9.2 = "$jetty_9_2;
-echo "Jetty 9.4 = "$jetty_9_4;
-echo "Jetty 10.0 = "$jetty_10_0;
-echo "Jetty 11.0 = "$jetty_11_0;
+  # print usage
+  usage
+  exit
+}
 
-#
-# prepare downloads
-#
-ARC_DIR="_archive";
-
-mkdir $ARC_DIR;
-
-#
-# prepare documentation
-#
-DOC_URL_BASE="https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-documentation"
-
-jetty_9_4_filename="jetty-documentation-$jetty_9_4-html.zip";
-wget -O $ARC_DIR/$jetty_9_4_filename "$DOC_URL_BASE/$jetty_9_4/$jetty_9_4_filename";
-
-
-#
-# cleanup downloads
-#
-# rm -Rf $ARC_DIR;
+main "$1"
