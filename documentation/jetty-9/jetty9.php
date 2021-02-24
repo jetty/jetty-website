d5bd80e5fc54b7a287d16081b690ae214e5863f8
Chris Walker <chris@webtide.com>
2021-02-24 16:46:28 -0600

    Doc Work MK.II
    Signed-off-by: Chris Walker <chris@webtide.com>

--- documentation/jetty-9/jetty9.php
+++ documentation/jetty-9/jetty9.php
@@ -39,7 +39,7 @@ $Theme->setThemeVariables($variables);
 
 // Place your html content in a file called content/en_pagename.php
 ob_start();
-include ("../../content/en_jetty_9_" . $App->getScriptName());
+include ("../../content/en_jetty_9_index" . $App->getScriptName());
 $html = ob_get_clean();
 $Theme->setHtml($html);
 
